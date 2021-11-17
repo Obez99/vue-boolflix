57e20740cb64543db180829ae57433fc64c78ef9
Obez99 <giuliogx@live.it>
2021-11-17 15:30:29 +0100

    Adds axios test call, prints found data in console

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
+  data() {
+    return {
+      apiKey: "031b25f0ecd29749a18d82fd3135886f",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: this.apiKey,
+          query: "ciao",
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+      });
+  },
 };
 </script>
 
