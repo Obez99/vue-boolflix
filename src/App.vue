52cc6646dbe6e7f5be3545bf6145812ae001c8eb
Obez99 <giuliogx@live.it>
2021-11-17 15:41:49 +0100

    Prints title, original title, language and rating of found movie in page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ul v-for="movie in currentMovies" :key="movie.id">
+      <li>Titolo: {{ movie.title }}</li>
+      <li>Titolo originale: {{ movie.original_title }}</li>
+      <li>Lingua: {{ movie.original_language }}</li>
+      <li>Voto: {{ movie.vote_average }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -9,6 +16,7 @@ export default {
   data() {
     return {
       apiKey: "031b25f0ecd29749a18d82fd3135886f",
+      currentMovies: [],
     };
   },
   mounted() {
@@ -16,11 +24,11 @@ export default {
       .get("https://api.themoviedb.org/3/search/movie", {
         params: {
           api_key: this.apiKey,
-          query: "ciao",
+          query: "Hulk",
         },
       })
       .then((response) => {
-        console.log(response.data.results);
+        this.currentMovies = response.data.results;
       });
   },
 };
