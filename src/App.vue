a9a3f012a77a08bb386142bb0777acbaf92b3526
Obez99 <giuliogx@live.it>
2021-11-18 12:45:18 +0100

    Adds Card component

--- src/App.vue
+++ src/App.vue
@@ -7,66 +7,36 @@
 
     <!-- Movie List !-->
     <h2>FILM</h2>
-    <ul v-for="movie in currentMovies" :key="movie.id">
-      <li>Titolo: {{ movie.title }}</li>
-      <li>Titolo originale: {{ movie.original_title }}</li>
-      <li>
-        Lingua:
-        <span
-          :class="
-            movie.original_language
-              ? `flag-icon flag-icon-${movie.original_language}`
-              : `flag-icon flag-icon-xx`
-          "
-        ></span
-        >{{ movie.original_language }}
-      </li>
-      <li>Voto: {{ movie.vote_average }}</li>
-      <li>
-        <img :src="imageUrl + movie.poster_path" :alt="movie.original_title" />
-      </li>
-    </ul>
+    <Card v-for="movie in currentMovies" :key="movie.id" :cardData="movie">
+    </Card>
     <h2>SERIE</h2>
-    <ul v-for="series in currentSeries" :key="series.id">
-      <li>Titolo: {{ series.name }}</li>
-      <li>Titolo originale: {{ series.original_name }}</li>
-      <li>
-        Lingua:
-        <span
-          :class="
-            series.original_language
-              ? `flag-icon flag-icon-${series.original_language}`
-              : `flag-icon flag-icon-xx`
-          "
-        ></span
-        >{{ series.original_language }}
-      </li>
-      <li>Voto: {{ series.vote_average }}</li>
-      <li>
-        <img :src="imageUrl + series.poster_path" :alt="series.original_name" />
-      </li>
-    </ul>
+    <Card
+      v-for="series in currentSeries"
+      :key="series.id"
+      :cardData="series"
+    ></Card>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Card from "./components/Card.vue";
 export default {
   name: "App",
+  components: { Card },
   data() {
     return {
-      apiUrl: "https://api.themoviedb.org/3/search/",
+      apiUrl: "https://api.themoviedb.org/3",
       apiKey: "031b25f0ecd29749a18d82fd3135886f",
       currentMovies: [],
       currentSeries: [],
       itemToSearch: "",
-      imageUrl: "https://image.tmdb.org/t/p/w342/",
     };
   },
   methods: {
     apiSearch(itemType, returnType) {
       axios
-        .get(this.apiUrl + itemType, {
+        .get(this.apiUrl + "/search/" + itemType, {
           params: {
             api_key: this.apiKey,
             query: this.itemToSearch,
---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li>Titolo: {{ cardData.title }}</li>
+    <li>Titolo originale: {{ cardData.original_title }}</li>
+    <li>
+      Lingua:
+      <span
+        :class="
+          cardData.original_language
+            ? `flag-icon flag-icon-${cardData.original_language}`
+            : `flag-icon flag-icon-xx`
+        "
+      ></span
+      >{{ cardData.original_language }}
+    </li>
+    <li>Voto: {{ cardData.vote_average }}</li>
+    <li>
+      <img
+        :src="imageUrl + cardData.poster_path"
+        :alt="cardData.original_title"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    cardData: Object,
+  },
+  data() {
+    return {
+      imageUrl: "https://image.tmdb.org/t/p/w342/",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
